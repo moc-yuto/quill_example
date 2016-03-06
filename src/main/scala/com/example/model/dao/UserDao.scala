3e1bd042543bbd4f7fe463855ac062e69f13adc6
Yuto Suzuki <yuutoo.advance@gmail.com>
2016-03-07 00:30:05 +0900

    add insert and transaction

--- src/main/scala/com/example/model/dao/UserDao.scala
+++ src/main/scala/com/example/model/dao/UserDao.scala
@@ -9,8 +9,14 @@ trait UserDao {
   val find = quote { (id: Int) =>
     query[Users].filter(u => u.id == id)
   }
-  val insert = quote {(id: Int, name: String) =>
-    query[Users].insert
+  val findName = quote{ (name: String) =>
+    query[Users].filter(_.name == name)
+  }
+  val insert = quote {(name: String, age: Int) =>
+    query[Users].insert(_.name -> name, _.age -> age)
+  }
+  val update = quote { (id: Int, age: Int) =>
+    query[Users].filter(_.id == id).update(_.age -> age)
   }
 
 }
--- src/main/scala/com/example/model/service/Users.scala
+++ src/main/scala/com/example/model/service/Users.scala
@@ -9,5 +9,19 @@ class Users extends DBConfig {
   def find(id: Int) = {
     db.run(UserDao.find)(id)
   }
+
+  /**
+    * an user name is to be unique.
+    * @param name
+    * @param age
+    * @return
+    */
+  def insertOrUpdate(name: String, age: Int) = {
+    db.transaction{
+      val a = db.run(UserDao.findName)(name).headOption
+      if (a.isEmpty) db.run(UserDao.insert)(name, age)
+      else db.run(UserDao.update)(a.get.id, age)
+    }
+  }
 }
 object Users extends Users
