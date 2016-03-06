6975a3c6de7e3c4134646a8af7e7a01f596be700
Yuto Suzuki <yuutoo.advance@gmail.com>
2016-03-07 00:40:44 +0900

    add sql

--- src/main/scala/com/example/model/dao/UserDao.scala
+++ src/main/scala/com/example/model/dao/UserDao.scala
@@ -2,8 +2,19 @@ package com.example.model.dao
 
 import io.getquill._
 
+
 case class Users(id: Int, name: String, age: Int)
 
+/**
+  * CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  */
 trait UserDao {
 
   val find = quote { (id: Int) =>
